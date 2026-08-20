b3bbcc09367296e13f612c92dd1c53c193fe818b
jayeshkum00-draft <jayeshkum00@gmail.com>
2026-08-20 10:11:23 +0530

    Update preprocess_dsec.py

--- utils/preprocess_dsec.py
+++ utils/preprocess_dsec.py
@@ -283,9 +283,9 @@ def process_sequence(seq, rgb_dir, events_dir, calibrations_dir, pairs_dir, prep
 def build_pairs(cfg: DictConfig):
     # Builds pairs of RGB and Event data based on timestamps and saves them in the specified output directory.
     dataset_path = Path(cfg.dsec_path)
-    out_root = Path(cfg.datasets.features_dir) / "preprocessed_dsec"
-    preprocessed_rgb_dir = Path(cfg.datasets.features_dir) / "preprocessed_dsec" / "rgb"
-    preprocessed_events_hist_dir = Path(cfg.datasets.features_dir) / "preprocessed_dsec" / "events" / "histogram"
+    out_root = Path(cfg.datasets.root_dir)
+    preprocessed_rgb_dir = Path(cfg.datasets.root_dir) / "rgb"
+    preprocessed_events_hist_dir = Path(cfg.datasets.root_dir) / "events" / "histogram"
     pairs_dir = out_root / "pairs_dsec"
 
     preprocessed_rgb_dir.mkdir(parents=True, exist_ok=True)
